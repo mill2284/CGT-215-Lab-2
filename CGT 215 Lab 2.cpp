5981b84aaecb384e40a3e42b040b11bf7b5e5e2a
Tyson Zane Miller <mill2284@purdue.edu>
2022-08-30 18:18:19 -0400

    Commit

--- CGT 215 Lab 2.cpp
+++ CGT 215 Lab 2.cpp
@@ -4,14 +4,26 @@
 #include <iostream>
 
 using namespace std;
+
 int main()
 {
 	float A;
 	float B;
 	float X;
+
+	//Having user input a value for A and assigning it to the variable A
 	cout << "Please enter a value for A: ";
 	cin >> A;
 	cout << "A=" << A << endl;
-	B = (A * 4) + 1;
-	cout << "And B=" << B << endl;
+
+	//Having user input a value for B and assigning it to the variable B
+	cout << "Please enter a value for B: ";
+	cin >> B;
+	cout << "B=" << B << endl;
+
+	//Telling user we are calculating for x, calculating it, then displaying the value of X
+	cout << "Calculating x for " << A << "x+" << B << "=0" << endl;
+	X = (-B) / A;
+	cout << "X=" << X << endl;
+
 }
