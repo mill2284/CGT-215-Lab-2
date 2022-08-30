07591c68e79b5ee7111dd57791f96a75647c854d
Tyson Zane Miller <mill2284@purdue.edu>
2022-08-30 17:37:07 -0400

    Add project files.

---
+++ CGT 215 Lab 2.cpp
@@ -0,0 +1,17 @@
+// CGT 215 Lab 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	float A;
+	float B;
+	float X;
+	cout << "Please enter a value for A: ";
+	cin >> A;
+	cout << "A=" << A << endl;
+	B = (A * 4) + 1;
+	cout << "And B=" << B << endl;
+}
